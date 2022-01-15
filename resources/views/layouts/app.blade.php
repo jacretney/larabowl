302dc5a82fcf3f603d36a5b6eb03a024d9030f6d
Jim <jacretney@gmail.com>
2022-01-15 15:23:48 +0000

    Set up tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Larabowl - @yield('title')</title>
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href="/css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
